d6bf60afdd25c85f9d89f1c8ebd506d638d9639d
lfgSammy <sammylovesdesign@gmail.com>
2026-07-06 16:37:12 +0100

    Add initializePaymentView with authentication permissions

--- payment/views.py
+++ payment/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+import hmac
+import hashlib
+import requests
+from django.conf import settings
+from django.utils import timezone
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.response import Response
+from .models import Payment
+from .serializers import PaymentSerializer
+from order.models import Order
+from users.models import Notification
 
-# Create your views here.
+class initializePaymentView(APIView):
+    permission_classes = [IsAuthenticated]
+
