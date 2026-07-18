4674e4473367a0a704f8b3980d7e39422e33a2ec
lfgSammy <sammylovesdesign@gmail.com>
2026-07-18 15:44:20 +0100

    Add OperatingHours model to manage restaurant operating times

--- users/models.py
+++ users/models.py
@@ -26,6 +26,21 @@ class User(AbstractUser):
     def is_customer(self):
         return self.role == 'customer' and not self.is_superuser
     
+
+class OperatingHours(models.Model):
+    DAY_CHOICES = [
+        (0, 'Monday'), (1, 'Tuesday'), (2, 'Wednesday'),
+        (3, 'Thursday'), (4, 'Friday'), (5, 'Saturday'), (6, 'Sunday'),
+    ]
+    day = models.IntegerField(choices=DAY_CHOICES, unique=True)
+    open_time = models.TimeField()
+    close_time = models.TimeField()
+    is_open = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"{self.get_day_display()} - {self.open_time} to {self.close_time}"
+
+    
 class Notification(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='notifications')
     message = models.TextField()
