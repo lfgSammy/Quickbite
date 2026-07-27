e9c659ad2f40a515e91ab0b573f1880b22c0095b
lfgSammy <sammylovesdesign@gmail.com>
2026-07-27 11:12:47 +0100

    Register OperatingHours model in admin panel

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import User, Notification
+from .models import User, Notification, OperatingHours
 
 admin.site.register(User)
+admin.site.register(OperatingHours)
 admin.site.register(Notification)
