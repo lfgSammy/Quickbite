75af8611b1f5952578421daf4acb404aaf21a0af
lfgSammy <sammylovesdesign@gmail.com>
2026-07-28 10:34:53 +0100

    Add email configuration for sending notifications via Resend

--- Quickbite/settings.py
+++ Quickbite/settings.py
@@ -90,6 +90,15 @@ PAYSTACK_SECRET_KEY = config('PAYSTACK_SECRET_KEY')
 PAYSTACK_PUBLIC_KEY = config('PAYSTACK_PUBLIC_KEY')
 
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.resend.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'resend'
+EMAIL_HOST_PASSWORD = config('RESEND_API_KEY')
+DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL', default='QuickBite <onboarding@resend.dev>') 
+
+
 ROOT_URLCONF = 'Quickbite.urls'
 
 TEMPLATES = [
