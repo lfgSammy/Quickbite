7a0ee64f9ae6d582aff84c75f3962b26b6e59a1c
lfgSammy <sammylovesdesign@gmail.com>
2026-07-30 15:31:02 +0100

    Add datetime import and handle pickup_time conversion in order notification

--- order/signals.py
+++ order/signals.py
@@ -3,11 +3,24 @@ from django.dispatch import receiver
 from .models import Order
 from users.models import Notification
 from django.contrib.auth import get_user_model
+import datetime
 
 User = get_user_model()
 
 @receiver(post_save, sender=Order)
 def handle_order_notification(sender, instance, created, **kwargs):
+    pickup_time = instance.pickup_time
+
+    if isinstance(pickup_time, str):
+        try:
+            pickup_time = datetime.datetime.fromisoformat(
+                pickup_time.replace('Z', '+00:00')
+            )
+        except ValueError:
+            pickup_time = None
+
+    pickup_str = pickup_time.strftime("%I:%M %p") if pickup_time else 'N/A'
+    
     if created:
         kitchen_staff = User.objects.filter(role='kitchen')
         for staff in kitchen_staff:
