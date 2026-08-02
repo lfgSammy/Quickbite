86f744216ddf967e3eb4bcbb0641eccdbf7ea404
lfgSammy <sammylovesdesign@gmail.com>
2026-08-02 17:48:31 +0100

    Update database configuration to use environment variables for connection settings

--- Quickbite/settings.py
+++ Quickbite/settings.py
@@ -122,10 +122,21 @@ WSGI_APPLICATION = 'Quickbite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': config('DB_NAME'),
+        'USER':config('DB_USER'),
+        'PASSWORD':config('PASSWORD'),
+        'HOST':config('DB_HOST'),
+        'PORT':config('DB_PORT')
     }
 }
 
