5d17d34896cf94236fef4354e06b4cb2c4c8dc4d
lfgSammy <sammylovesdesign@gmail.com>
2026-08-07 11:14:52 +0100

    Refactor database configuration to support dynamic DATABASE_URL and fallback to SQLite

--- Quickbite/settings.py
+++ Quickbite/settings.py
@@ -147,24 +147,23 @@ WSGI_APPLICATION = 'Quickbite.wsgi.application'
 
 DATABASE_URL= config('DATABASE_URL', default=None)
 
-# if DATABASE_URL:
-DATABASES = {
+DATABASE_URL = config('DATABASE_URL', default=None)
+
+if DATABASE_URL:
+    DATABASES = {
         'default': dj_database_url.config(
-            default=f"sqlite:///{BASE_DIR/ 'dbsqlite3'}",
+            default=DATABASE_URL,
             conn_max_age=600,
+            ssl_require=True
         )
     }
-# else:
-#     DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.db.backends.postgresql',
-#             'NAME': config('DB_NAME'),
-#             'USER': config('DB_USER'),
-#             'PASSWORD': config('DB_PASSWORD'),
-#             'HOST': config('DB_HOST'),
-#             'PORT': config('DB_PORT'),
-#         }
-#     }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
