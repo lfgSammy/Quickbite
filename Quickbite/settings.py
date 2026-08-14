ff4eb5602b69681c93c6e6e30f50808dd650ce29
lfgSammy <sammylovesdesign@gmail.com>
2026-08-14 15:49:59 +0100

    Update DEBUG setting to use environment variable for improved security

--- Quickbite/settings.py
+++ Quickbite/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(arv5_0l!o(j_ev1#zm#+k$$!c5kc8my_eala&z@&5a$o6h5&2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool, default=False)
 
 ALLOWED_HOSTS = ["*"]
 CORS_ALLOWED_ORIGINS= ['http://localhost:3000',]
