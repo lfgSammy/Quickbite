72d0004978b27b2025b3f902c55c8e6a5febfe91
lfgSammy <sammylovesdesign@gmail.com>
2026-08-20 11:23:14 +0100

    Change order field in Payment model from OneToOneField to ForeignKey

--- payment/models.py
+++ payment/models.py
@@ -7,7 +7,7 @@ class Payment(models.Model):
         ('success', 'Success'),
         ('failed', 'Failed')
     ]
-    order = models.OneToOneField(Order, on_delete=models.CASCADE, related_name='payment')
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='payments')
     paystack_reference = models.CharField(max_length=100, unique=True)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     status = models.CharField(max_length=12, choices=PAYMENT_STATUS, default='pending')
