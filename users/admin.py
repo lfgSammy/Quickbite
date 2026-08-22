0cb767636fe7152963e339abad7a1bbe731e8ab5
lfgSammy <sammylovesdesign@gmail.com>
2026-08-22 17:58:33 +0100

    Register PasswordResetOTP model in admin panel

--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import User, Notification, OperatingHours
+from .models import User, Notification, OperatingHours, PasswordResetOTP
 
 admin.site.register(User)
+admin.site.register(PasswordResetOTP)
 admin.site.register(OperatingHours)
 admin.site.register(Notification)
