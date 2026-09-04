008d0545f2195e9832e09a276eca19523949627a
lfgSammy <sammylovesdesign@gmail.com>
2026-09-04 14:41:45 +0100

    Refactor views to add serializer classes for menu items and related entities

--- menu/views.py
+++ menu/views.py
@@ -8,10 +8,10 @@ from .serializers import (MenuItemSerializer, MenuItemSizeSerializer,
                            ShawarmaOptionSerializer, RiceTypeSerializer,
                            RiceExtraSerializer, ShawarmaExtraSerializer,
                            DrinkSerializer)
-from drf_spectacular.utils import extend_schema
 
 class MenuItemListView(APIView):
     permission_classes = [IsAdminOrReadOnly]
+    serializer_class = MenuItemSerializer
 
     def get(self, request):
         items = MenuItem.objects.prefetch_related(
@@ -25,7 +25,6 @@ class MenuItemListView(APIView):
         serializer = MenuItemSerializer(items, many=True)
         return Response(serializer.data)
 
-    @extend_schema(request=MenuItemSerializer)
     def post(self, request):
         serializer = MenuItemSerializer(data=request.data)
         if serializer.is_valid():
@@ -35,6 +34,7 @@ class MenuItemListView(APIView):
 
 class MenuItemDetailView(APIView):
     permission_classes = [IsAdminOrReadOnly]
+    serializer_class = MenuItemSerializer
 
     def get_object(self, pk):
         try:
@@ -51,7 +51,6 @@ class MenuItemDetailView(APIView):
         serializer = MenuItemSerializer(item)
         return Response(serializer.data)
 
-    @extend_schema(request=MenuItemSerializer)
     def patch(self, request, pk):
         item = self.get_object(pk)
         if not item:
@@ -74,13 +73,13 @@ class MenuItemDetailView(APIView):
 
 class MenuItemSizeListView(APIView):
     permission_classes = [IsAdminOrReadOnly]
+    serializer_class = MenuItemSizeSerializer
 
     def get(self, request, menu_item_id):
         sizes = MenuItemSize.objects.filter(menu_item_id=menu_item_id)
         serializer = MenuItemSizeSerializer(sizes, many=True)
         return Response(serializer.data)
 
-    @extend_schema(request=MenuItemSizeSerializer)
     def post(self, request, menu_item_id):
         menu_item = MenuItem.objects.filter(
             id=menu_item_id, item_type='rice').first()
@@ -96,6 +95,7 @@ class MenuItemSizeListView(APIView):
 
 class MenuItemSizeDetailView(APIView):
     permission_classes = [IsAdminOrReadOnly]
+    serializer_class = MenuItemSizeSerializer
 
     def get_object(self, pk):
         try:
@@ -103,7 +103,6 @@ class MenuItemSizeDetailView(APIView):
         except MenuItemSize.DoesNotExist:
             return None
 
-    @extend_schema(request=MenuItemSizeSerializer)
     def patch(self, request, pk):
         size = self.get_object(pk)
         if not size:
@@ -126,13 +125,13 @@ class MenuItemSizeDetailView(APIView):
 
 class ShawarmaOptionListView(APIView):
     permission_classes = [IsAdminOrReadOnly]
+    serializer_class = ShawarmaOptionSerializer
 
     def get(self, request, menu_item_id):
         options = ShawarmaOption.objects.filter(menu_item_id=menu_item_id)
         serializer = ShawarmaOptionSerializer(options, many=True)
         return Response(serializer.data)
 
-    @extend_schema(request=ShawarmaOptionSerializer)
     def post(self, request, menu_item_id):
         menu_item = MenuItem.objects.filter(
             id=menu_item_id, item_type='shawarma').first()
@@ -148,6 +147,7 @@ class ShawarmaOptionListView(APIView):
 
 class ShawarmaOptionDetailView(APIView):
     permission_classes = [IsAdminOrReadOnly]
+    serializer_class = ShawarmaOptionSerializer
 
     def get_object(self, pk):
         try:
@@ -155,7 +155,6 @@ class ShawarmaOptionDetailView(APIView):
         except ShawarmaOption.DoesNotExist:
             return None
 
-    @extend_schema(request=ShawarmaOptionSerializer)
     def patch(self, request, pk):
         option = self.get_object(pk)
         if not option:
@@ -178,13 +177,13 @@ class ShawarmaOptionDetailView(APIView):
 
 class RiceTypeListView(APIView):
     permission_classes = [IsAdminOrReadOnly]
+    serializer_class = RiceTypeSerializer
 
     def get(self, request):
         rice_types = RiceType.objects.all()
         serializer = RiceTypeSerializer(rice_types, many=True)
         return Response(serializer.data)
 
-    @extend_schema(request=RiceTypeSerializer)
     def post(self, request):
         serializer = RiceTypeSerializer(data=request.data)
         if serializer.is_valid():
@@ -195,6 +194,7 @@ class RiceTypeListView(APIView):
 
 class RiceTypeDetailView(APIView):
     permission_classes = [IsAdminOrReadOnly]
+    serializer_class = RiceTypeSerializer
 
     def get_object(self, pk):
         try:
@@ -202,7 +202,6 @@ class RiceTypeDetailView(APIView):
         except RiceType.DoesNotExist:
             return None
 
-    @extend_schema(request=RiceTypeSerializer)
     def patch(self, request, pk):
         rice_type = self.get_object(pk)
         if not rice_type:
@@ -225,13 +224,13 @@ class RiceTypeDetailView(APIView):
 
 class RiceExtraListView(APIView):
     permission_classes = [IsAdminOrReadOnly]
+    serializer_class = RiceExtraSerializer
 
     def get(self, request):
         extras = RiceExtra.objects.filter(is_available=True)
         serializer = RiceExtraSerializer(extras, many=True)
         return Response(serializer.data)
 
-    @extend_schema(request=RiceExtraSerializer)
     def post(self, request):
         serializer = RiceExtraSerializer(data=request.data)
         if serializer.is_valid():
@@ -242,6 +241,7 @@ class RiceExtraListView(APIView):
 
 class RiceExtraDetailView(APIView):
     permission_classes = [IsAdminOrReadOnly]
+    serializer_class = RiceExtraSerializer
 
     def get_object(self, pk):
         try:
@@ -249,7 +249,6 @@ class RiceExtraDetailView(APIView):
         except RiceExtra.DoesNotExist:
             return None
 
-    @extend_schema(request=RiceExtraSerializer)
     def patch(self, request, pk):
         extra = self.get_object(pk)
         if not extra:
@@ -272,13 +271,13 @@ class RiceExtraDetailView(APIView):
 
 class ShawarmaExtraListView(APIView):
     permission_classes = [IsAdminOrReadOnly]
+    serializer_class = ShawarmaExtraSerializer
 
     def get(self, request):
         extras = ShawarmaExtra.objects.filter(is_available=True)
         serializer = ShawarmaExtraSerializer(extras, many=True)
         return Response(serializer.data)
 
-    @extend_schema(request=ShawarmaExtraSerializer)
     def post(self, request):
         serializer = ShawarmaExtraSerializer(data=request.data)
         if serializer.is_valid():
@@ -289,6 +288,7 @@ class ShawarmaExtraListView(APIView):
 
 class ShawarmaExtraDetailView(APIView):
     permission_classes = [IsAdminOrReadOnly]
+    serializer_class = ShawarmaExtraSerializer
 
     def get_object(self, pk):
         try:
@@ -296,7 +296,6 @@ class ShawarmaExtraDetailView(APIView):
         except ShawarmaExtra.DoesNotExist:
             return None
 
-    @extend_schema(request=ShawarmaExtraSerializer)
     def patch(self, request, pk):
         extra = self.get_object(pk)
         if not extra:
@@ -319,13 +318,13 @@ class ShawarmaExtraDetailView(APIView):
 
 class DrinkListView(APIView):
     permission_classes = [IsAdminOrReadOnly]
+    serializer_class = DrinkSerializer
 
     def get(self, request):
         drinks = Drink.objects.filter(is_available=True)
         serializer = DrinkSerializer(drinks, many=True)
         return Response(serializer.data)
 
-    @extend_schema(request=DrinkSerializer)
     def post(self, request):
         serializer = DrinkSerializer(data=request.data)
         if serializer.is_valid():
@@ -336,6 +335,7 @@ class DrinkListView(APIView):
 
 class DrinkDetailView(APIView):
     permission_classes = [IsAdminOrReadOnly]
+    serializer_class = DrinkSerializer
 
     def patch(self, request, pk):
         try:
