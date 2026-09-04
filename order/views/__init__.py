9bd60fb22df8b617ddd52d18bf21232f10aa4c13
lfgSammy <sammylovesdesign@gmail.com>
2026-09-04 14:42:56 +0100

    Add CartItemDetailView to the views module exports

--- order/views/__init__.py
+++ order/views/__init__.py
@@ -1,11 +1,12 @@
-from .cart import (CartView, CartItemView, UpdateCartItemView,
-                   RevertOrderToCartView)
+from .cart import (CartView, CartItemView, CartItemDetailView,
+                   UpdateCartItemView, RevertOrderToCartView)
 from .order_item import OrderListView, OrderDetailView, CancelOrderView
 from .verifyQR import VerifyQRView, AdminDashboardView
 
 __all__ = [
     'CartView',
     'CartItemView',
+    'CartItemDetailView',
     'UpdateCartItemView',
     'RevertOrderToCartView',
     'OrderListView',
